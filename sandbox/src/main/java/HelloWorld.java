468b5231e431139a723f637eb6f5b531e3a3380c
alexeykrutin <rhenby@gmail.com>
2017-03-13 22:27:39 +0700

    Задание 2, пункты 1-3

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
-  
-  public static void main(String[] args){
-    System.out.println("Hello World!");
-  }
 
+    public static void main(String[] args){
+        Point p1 = new Point(-1, 3);
+        Point p2 = new Point(6, 2);
+        System.out.println("Расстояние между точками с координатами A("
+                + p1.x + ", " + p2.x + ") и B(" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
+    }
+
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
 }
