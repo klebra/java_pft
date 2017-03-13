631adfadb31b40413139d6e9d1b0e4759454ee69
alexeykrutin <rhenby@gmail.com>
2017-03-13 23:01:34 +0700

    Функция перенесена в объект

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
         Point p1 = new Point(2, 2);
         Point p2 = new Point(2, 6);
         System.out.println("Расстояние между точками с координатами A("
-                + p1.x + ", " + p1.y + ") и B(" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
+                + p1.x + ", " + p1.y + ") и B(" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
     }
 
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
-    }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -6,4 +6,8 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public double distance(Point p){
+        return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+    }
 }
