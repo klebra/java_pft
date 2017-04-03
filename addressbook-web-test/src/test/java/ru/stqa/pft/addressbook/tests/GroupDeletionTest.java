b1d0630bbcc3a9cd540df840e53d0155825bc392
alexeykrutin <rhenby@gmail.com>
2017-04-03 22:41:07 +0700

    - добавлено сравнение списков для тестов модификации и удаления групп

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -4,20 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTest extends TestBase{
 
     @Test
     public void testGroupDeletion(){
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         if (!app.getGroupHelper().isGroupPresent()){
             app.getGroupHelper().createGroup(new GroupData("Name of group", null , null));
         }
-        app.getNavigationHelper().selectCheckbox(before - 1);
+        app.getNavigationHelper().selectCheckbox(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before - 1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
     }
 
 }
