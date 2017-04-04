0adc507495cd9ec16ab1861b9cf509ffb24be843
alexeykrutin <rhenby@gmail.com>
2017-04-04 22:56:25 +0700

    - реализовано сравнение списков групп как неупорядоченных коллекций

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditingTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditingTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupEditingTest extends TestBase{
@@ -17,10 +18,15 @@ public class GroupEditingTest extends TestBase{
         }
         app.getNavigationHelper().selectCheckbox(before.size() - 1);
         app.getGroupHelper().editGroup();
-        app.getGroupHelper().fillGroupForm(new GroupData("Group of name", "Footer", "Header"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Group of name", "Footer", "Header");
+        app.getGroupHelper().fillGroupForm(group);
         app.getNavigationHelper().clickOnUpdate();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
