88ed159dcd8af285f69fc81aaa8caafd66b6f0fa
alexeykrutin <rhenby@gmail.com>
2017-04-05 08:28:56 +0700

    - добавлено сравнение списков для создания группы и присвоение id создаваемому элементу списка

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -14,7 +14,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -25,17 +25,6 @@ public class GroupData {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
     public String toString() {
         return "GroupData{" +
                 "id='" + id + '\'' +
@@ -43,15 +32,12 @@ public class GroupData {
                 '}';
     }
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+    public int getId() {
+        return id;
     }
 
-    public String getId() {
-        return id;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getHeader() {
@@ -61,4 +47,23 @@ public class GroupData {
     public String getFooter() {
         return footer;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
 }
