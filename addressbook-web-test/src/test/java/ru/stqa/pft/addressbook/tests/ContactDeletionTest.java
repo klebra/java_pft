3d40f3dac248aecc5cab47bfd40bc14057c7baeb
alexeykrutin <rhenby@gmail.com>
2017-04-06 08:44:41 +0700

    - Ответ на 10 задание

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,18 +1,28 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeletionTest extends TestBase{
 
     @Test
     public void testContactDeletion(){
         app.getNavigationHelper().goToHomePage();
+        List<ContactData> before = app.getContactsHelper().getContactList();
         if (!app.getContactsHelper().isContactPresent()){
             app.getContactsHelper().createContact(new ContactData("Name", null, null, null, null));
         }
-        app.getNavigationHelper().selectCheckbox(0);
+        app.getNavigationHelper().selectCheckbox(before.size() - 1);
         app.getContactsHelper().deleteContact();
         app.getNavigationHelper().acceptAlert();
+        app.getNavigationHelper().goToHomePage();
+        List<ContactData> after = app.getContactsHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 }
