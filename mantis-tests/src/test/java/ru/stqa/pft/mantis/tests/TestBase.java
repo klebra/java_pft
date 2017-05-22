e94f2b76a584d6e0cad17a767042ee982da4dffd
alexeykrutin <rhenby@gmail.com>
2017-05-22 13:56:18 +0700

    - ответ на 21 задание

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+import ru.stqa.pft.mantis.model.Issue;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -25,4 +30,14 @@ public class TestBase {
         app.stop();
     }
 
+    public boolean isIssueOpen(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        Issue issue = app.soap().getIssue(issueId);
+        return !issue.getStatus().equals("resolved");
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
 }
