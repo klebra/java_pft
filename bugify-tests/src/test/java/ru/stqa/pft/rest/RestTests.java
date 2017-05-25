ecdde0a6a93b18dbb7c9cdf289a056f354c1c479
alexeykrutin <rhenby@gmail.com>
2017-05-25 11:31:22 +0700

    - исправлено 22 задание

--- bugify-tests/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ bugify-tests/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,13 +1,5 @@
 package ru.stqa.pft.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
-import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -15,7 +7,7 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
     @Test
     public void testCreateIssue() throws IOException {
@@ -28,53 +20,4 @@ public class RestTests {
         assertEquals(newIssues, oldIssues);
     }
 
-    private Set<Issue> getIssue() throws IOException {
-        String json = getExecutor()
-                .execute(Request.Get("http://demo.bugify.com/api/issues.json"))
-                .returnContent()
-                .asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-    }
-
-    private Set<Issue> issue(int issueId) throws IOException {
-        String json = getExecutor()
-                .execute(Request.Get(String.format("http://demo.bugify.com/api/issues/%s.json", issueId)))
-                .returnContent()
-                .asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-    }
-
-    private Executor getExecutor() {
-        return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
-    }
-
-    private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor()
-                .execute(Request
-                        .Post("http://demo.bugify.com/api/issues.json")
-                        .bodyForm(
-                                new BasicNameValuePair("subject", newIssue.getSubject()),
-                                new BasicNameValuePair("description", newIssue.getDescription())
-                        )
-                )
-                .returnContent()
-                .asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        return parsed.getAsJsonObject().get("issue_id").getAsInt();
-    }
-
-    public boolean isIssueOpen(int issueId) throws IOException {
-        Issue issue = issue(issueId).iterator().next();
-        return (issue.getState_name().equals("Open") || issue.getState_name().equals("In Progress"));
-    }
-
-    public void skipIfNotFixed(int issueId) throws IOException {
-        if (isIssueOpen(issueId)) {
-            throw new SkipException("Ignored because of issue " + issueId);
-        }
-    }
 }
